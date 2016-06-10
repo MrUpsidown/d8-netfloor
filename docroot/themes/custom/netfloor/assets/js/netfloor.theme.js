4cedefb798549ee55f5ba2c4937e9bee5c2571a6
Arnaud Marquis <arnaud.marquis@ibrows.ch>
2016-06-10 12:30:29 +0200

    d8

---
+++ docroot/themes/custom/netfloor/assets/js/netfloor.theme.js
@@ -0,0 +1,16 @@
+;
+
+(function ($) {
+
+    "use strict";
+
+    Drupal.behaviors.netfloor = {
+        attach: function (context, settings) {
+
+            "use strict";
+
+            console.log('netfloor theme');
+        }
+    }
+    ;
+})(jQuery);
